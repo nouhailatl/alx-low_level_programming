813557362eeb9c11813b990fda1eacb5e5ad76c3
nouhaila <nouhailatouil7@gmail.com>
2023-06-22 18:23:02 -0700

    jejfb

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+	if(i > 0)
+		printf("%d is positive", i);
+	else if(i < 0)
+                printf("%d is negative", i);
+	else
+		printf("% is zero", i);
+}
